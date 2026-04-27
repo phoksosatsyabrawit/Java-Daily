09648e4acd757d72b41575b169d58fee361f96e4
local.brawit <phoksosatsyabrawit@gmail.com>
2026-04-27 22:24:16 +0700

    update animalapplication

---
+++ inheritance/animalapplication/Animal.java
@@ -0,0 +1,15 @@
+package com.java.inheritance.animalapplication;
+
+public class Animal {
+     protected String name;
+
+     void setName(String name){
+        this.name = name;
+     }
+     void eat(){
+        System.out.println("Animal can eat.");
+     }
+     void showName(){
+        System.out.println("Dog name: " + name);
+     }
+}
---
+++ inheritance/animalapplication/Application.java
@@ -0,0 +1,19 @@
+package com.java.inheritance;
+
+import java.text.Annotation;
+
+public class Application {
+    public static void main(String[] args){
+        Animal animal = new Animal();
+        //animal.name = "wang cai";
+        //animal.eat();
+        //animal.showName();
+        
+
+        Dog dog = new Dog();
+        //dog.setName("letty");
+        //dog.showInfo();
+        dog.eatAll();
+        //dog.showName();
+    }
+}
