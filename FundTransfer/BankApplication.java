895acfe7c7f15802f341f6d029281f46ee4846b0
local.brawit <phoksosatsyabrawit@gmail.com>
2026-05-05 17:14:20 +0700

    updated entity name

--- FundTransfer/BankApplication.java
+++ FundTransfer/BankApplication.java
@@ -2,8 +2,7 @@ package com.java.FundTransfer;
 
 public class BankApplication {
     public static void main(String[] args){
-        int numberofAccounts = 3;
-        Bank bank = new Bank("E Crop", Bank.openAccount(numberofAccounts));
+        Bank bank = new Bank("E Crop", Bank.openAccount(3));
         bank.display();
         bank.transfer("000000000001", "000000000002", 150);
         System.out.println("\n #After transfering $150 from Elliot to Darlene...\n");
